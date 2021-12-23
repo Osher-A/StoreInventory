5dd6357b6d44ddf0f7a5be994472336dee91aa81
Osher Moscovitch <oamoscovitch@gmail.com>
2021-12-23 01:13:15 +0000

    Create Email template

---
+++ Unit Tests/ServicesTests/OrderServiceTests/OrderDataServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.ServicesTests.OrderServiceTests
+{
+    [TestFixture]
+    internal class OrderDataServiceTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+        [Test]
+        public void SaveOrderDetails_WhenAllCustomersDetailsAreNullAndFullyPaid_DoNotAddNewCustomerInRepo()
+        {
+
+        }
+       
+        [Test]
+        public void SaveOrderDetails_WhenPartOfDetailsNotNullAndCanBeMatchedToExistingCustomer_UpdateCustomerInRepo()
+        {
+
+        }
+        [Test]
+        public void SaveOrderDetails_WhenPartOfDetailsNotNullAndCantBeMatchedToExistingCustomer_AddNewCustomerInRepo()
+        {
+
+        }
+
+    }
+}
